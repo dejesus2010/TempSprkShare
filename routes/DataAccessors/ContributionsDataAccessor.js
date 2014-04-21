49f04e9bddec52762866bc1def1d2822c2ddff18
dejesus2010 <dejesus2010@gmail.com>
2014-04-21 06:56:35 -0400

    Contributing now shows contributions with images

--- routes/DataAccessors/ContributionsDataAccessor.js
+++ routes/DataAccessors/ContributionsDataAccessor.js
@@ -54,12 +54,14 @@ var constructor = function(){
 
         var preparedStatement = 'INSERT INTO contributions(contribpostid, contribuserid, contribcontent, contribhasmedia, contributeddate ) ' +
             'VALUES ($1, $2, $3, false, current_date )' +
-            'RETURNING *, (SELECT username FROM sparkusers WHERE sparkusers.userid = contribuserid)';
+            'RETURNING *, (SELECT username FROM sparkusers WHERE sparkusers.userid = contribuserid), (SELECT userpicurl FROM sparkusers WHERE sparkusers.userid = contribuserid)';
         var inserts = [contributionData.postId, contributionData.userId, contributionData.content];
 
         pg.connect(process.env.DATABASE_URL, function(err, client, done){
             client.query(preparedStatement, inserts, function(err, result){
                 done();
+
+                console.log(result.rows[0]);
 
                 // TODO finish implementation
                 if( err ){
