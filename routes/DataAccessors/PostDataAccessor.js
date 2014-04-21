65203340320680edc3890bf9ab5c50e5a34666c8
aklintsevich <aklintsevich@ufl.edu>
2014-04-21 01:26:52 -0400

    fixed input in posts

--- routes/DataAccessors/PostDataAccessor.js
+++ routes/DataAccessors/PostDataAccessor.js
@@ -4,8 +4,8 @@ var constructor = function(){
 	var pg = require('pg');
 	userPostInstance.insert = function(data , sendData){
 		
-		var preparedStatement = 'INSERT INTO posts( PostTitle, PostContent, PostHasMedia) VALUES ($1, $2, $3) RETURNING PostId';
-		var inserts = [ data.title, data.message, data.hasPicture];
+		var preparedStatement = 'INSERT INTO posts( PostTitle, PostContent, PostHasMedia, PostShareCount, PostShareQuotaId, PostedDate) VALUES ($1, $2, $3, $4, $5, current_date) RETURNING PostId';
+		var inserts = [ data.title, data.message, data.hasPicture , 0, 1];
 		
 		 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 						console.log(process.env.DATABASE_URL);
